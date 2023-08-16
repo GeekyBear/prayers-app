6773a7ea5a4ff97ca8a7053719347cdb95f137b8
unknown <ezegeek@gmail.com>
2023-08-16 10:54:15 -0300

    Minor changes

--- App.js
+++ App.js
@@ -1,83 +1,114 @@
-import { StyleSheet, Text } from 'react-native';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useFonts } from 'expo-font';
+import { StyleSheet, Text } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useFonts } from "expo-font";
 
 // Navigation
 const Tab = createBottomTabNavigator();
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 // Components
-import ChurchPrayers from './components/ChurchPrayers/ChurchPrayers';
-import MyPrayers from './components/MyPrayers/MyPrayers';
-import MyProfile from './components/MyProfile/MyProfile';
+import ChurchPrayers from "./components/ChurchPrayers/ChurchPrayers";
+import MyPrayers from "./components/MyPrayers/MyPrayers";
+import MyProfile from "./components/MyProfile/MyProfile";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'Lora-Regular': require('./assets/fonts/Lora-Regular.ttf'),
+    "Lora-Regular": require("./assets/fonts/Lora-Regular.ttf"),
   });
 
   const [data, setData] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const fetchData = () => {
-    const baseURL = "http://192.168.100.200:3000/prays";
-    axios.get(`${baseURL}`).then((response) => setData(response.data.data));
+    setIsLoading(true);
+
+    const baseURL = "http://192.168.100.200:3001/prays";
+
+    try {
+      axios
+        .get(`${baseURL}`)
+        .then((response) => setData(response.data.data))
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const activeIcon = '#718e63';
-  const inactiveIcon = '#b3b4b4';
+  const activeIcon = "#718e63";
+  const inactiveIcon = "#b3b4b4";
 
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
           tabBarShowLabel: false,
-          tabBarLabelPosition: 'beside-icon',
+          tabBarLabelPosition: "beside-icon",
           tabBarLabelStyle: {
             fontSize: 16,
           },
           tabBarStyle: {
-            height: '7%',
-            justifyContent: 'space-around',
-            alignItems: 'flex-start',
-            borderTopColor: 'rgba(0, 0, 0, .2)',
+            height: "7%",
+            justifyContent: "space-around",
+            alignItems: "flex-start",
+            borderTopColor: "rgba(0, 0, 0, .2)",
           },
         }}
       >
-        <Tab.Screen name="Mi lista"
+        <Tab.Screen
+          name="Mi lista"
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name="hands-pray" color={focused ? activeIcon : inactiveIcon} size={24} />
+              <MaterialCommunityIcons
+                name="hands-pray"
+                color={focused ? activeIcon : inactiveIcon}
+                size={24}
+              />
             ),
           }}
-          component={MyPrayers} />
-        <Tab.Screen name="Nuestra lista"
+          component={MyPrayers}
+        />
+        <Tab.Screen
+          name="Nuestra lista"
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name="church" color={focused ? activeIcon : inactiveIcon} size={24} />
+              <MaterialCommunityIcons
+                name="church"
+                color={focused ? activeIcon : inactiveIcon}
+                size={24}
+              />
             ),
           }}
-          children={() => <ChurchPrayers data={data} />} />
-        <Tab.Screen name="Mi perfil"
+          children={() => <ChurchPrayers data={data} isLoading={isLoading} />}
+        />
+        <Tab.Screen
+          name="Mi perfil"
           options={{
             headerShown: false,
             tabBarIcon: ({ focused }) => (
-              <MaterialCommunityIcons name="account" color={focused ? activeIcon : inactiveIcon} size={24} />
+              <MaterialCommunityIcons
+                name="account"
+                color={focused ? activeIcon : inactiveIcon}
+                size={24}
+              />
             ),
           }}
-          component={MyProfile} />
+          component={MyProfile}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
--- components/ChurchPrayers/ChurchPrayers.js
+++ components/ChurchPrayers/ChurchPrayers.js
@@ -1,53 +1,60 @@
 import React from "react";
-import { Text, View, StyleSheet, Dimensions, StatusBar, Image } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  Image,
+} from "react-native";
 
 import List from "../List/List";
 
-const width = Dimensions.get('window').width; //full width
-const height = Dimensions.get('window').height; //full height
+const width = Dimensions.get("window").width; //full width
+const height = Dimensions.get("window").height; //full height
 
-export default function ChurchPrayers({ data }) {
-    console.log(data)
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.sectionTitle}>Motivos de la iglesia</Text>
-            </View>
-            {data.length === 0 && <Text>Cargando...</Text>}
-            {data && <List data={data} title={'Church'} />}
-            <StatusBar style="auto" />
-        </View>
-    );
-};
+export default function ChurchPrayers({ data, isLoading }) {
+  console.log(data);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.sectionTitle}>Motivos de la iglesia</Text>
+      </View>
+      {isLoading && <Text>Cargando...</Text>}
+      {data && <List data={data} title={"Church"} />}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    header: {
-        backgroundColor: '#f6f5f4',
-        width: '100%',
-        height: '10%',
-        marginTop: 16,
-        alignItems: 'flex-start',
-        justifyContent: 'center'
-    },
-    sectionTitle: {
-        paddingLeft: 16,
-        fontWeight: 400,
-        color: 'black',
-        fontFamily: 'Lora-Regular',
-        fontSize: 30
-    },
-    howMany: {
-        fontSize: 15,
-        fontWeight: 400,
-        color: '#939191',
-    },
-    icon: {
-        width: 50,
-        height: 50,
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "#f6f5f4",
+    width: "100%",
+    height: "10%",
+    marginTop: 16,
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  sectionTitle: {
+    paddingLeft: 16,
+    fontWeight: 400,
+    color: "black",
+    fontFamily: "Lora-Regular",
+    fontSize: 30,
+  },
+  howMany: {
+    fontSize: 15,
+    fontWeight: 400,
+    color: "#939191",
+  },
+  icon: {
+    width: 50,
+    height: 50,
+  },
 });
